ac1694f39b40ee1fdfefead1c1d0a18a016596a8
mohandonga201 <mohandonga9951@gmail.com>
2024-05-05 06:43:15 -0400

    update_switch_script.sh

---
+++ switch_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "enter activity input:"
+read key
+case $key in
+    start)
+     echo "start the service"
+     ;;
+    stop)
+     echo "stop the service"
+     ;;
+    restart)
+     echo "restart the service"
+     ;;
+    status)
+     echo "to check httpd service"
+     service httpd status
+     ;;
+    *)
+     echo "option not valid, choose start/stop/restart wisely"
+     ;;
+esac
